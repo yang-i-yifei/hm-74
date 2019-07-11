f52ff58f8ce9d3e98fe57ae0e7769069cfd3f355
yang-i-yifei <qiannianhuashen@qq.com>
2019-07-11 23:27:55 +0800

    创建login页面

--- herolistforspring/src/main.js
+++ herolistforspring/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入包
+// 导入样式
+import router from './router/router.js'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册到VUE实例
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ herolistforspring/src/router/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+import Login from '../views/login/login.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { name: 'login', path: '/login', component: Login }
+  ] // 配置路由规则
+})
+export default router
