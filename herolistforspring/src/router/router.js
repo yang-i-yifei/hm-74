feb9228802122c98a6077cecbcc34340b49dc95b
yang-i-yifei <qiannianhuashen@qq.com>
2019-07-12 11:15:25 +0800

    login.modify

--- herolistforspring/src/router/router.js
+++ herolistforspring/src/router/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 
 import VueRouter from 'vue-router'
 import Login from '../views/login/login.vue'
+import Home from '../views/login/home.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    { name: 'home', path: '/', component: Home }
   ] // 配置路由规则
 })
 export default router
--- herolistforspring/src/views/login/login.vue
+++ herolistforspring/src/views/login/login.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <el-card class="login-box">
       <img src="../../assets/logo_index.png" alt />
-      <el-form ref="loginForm" :model="loginForm">
+      <el-form ref="loginForm" :status-icon="true" :model="loginForm" :rules="loginRules">
         <!-- 表单每一项的容器 label="活动名称" 文字说明-->
-        <el-form-item>
+        <el-form-item prop="mobile">
           <!-- 表单元素 -->
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- 表单元素 -->
           <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
           <el-button style="float:right">发送验证码</el-button>
@@ -17,7 +17,7 @@
           <el-checkbox :value="true" >我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%" type="primary">登 录</el-button>
+          <el-button style="width:100%" type="primary" @click="login()">登 录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -26,15 +26,52 @@
 
 <script>
 export default {
-  data() {
+  data () {
+    // 校验手机号
+    const checkMobile = (rule, value, callback) => {
+      // 校验逻辑   把value拿出来进行手机号的格式校验
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不正确'))
+      }
+    }
     return {
       loginForm: {
-        mobile: "",
-        code: ""
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入正确的手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
       }
-    };
+    }
+  },
+  methods: {
+    login () {
+      this.refs.loginForm.loginForm(valid => {
+        if (valid) {
+          this.$http
+            .post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
   }
-};
+
+}
 </script>
 
 <style scoped lang='less'>
